53ea0f87985f8ec65c3ac3e958948e34389a3640
MeTheAlexo <methealexost@gmail.com>
2024-12-23 22:49:27 +0300

    shadcn

---
+++ src/lib/fonts.ts
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+
+import { Geist as FontSans, Geist_Mono as FontMono } from 'next/font/google'
+
+export const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans'
+})
+
+export const fontMono = FontMono({
+  subsets: ['latin'],
+  variable: '--font-mono'
+})
